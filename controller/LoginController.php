66a5bc534a7814e092736c522a333188235259fc
yedai13 <facundo.marin2012@gmail.com>
2021-06-19 20:28:33 -0300

    Login redireccionando index

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -21,7 +21,7 @@ class LoginController
 
         $email = $_POST["email"];
         $password = $_POST["password"];
-        $data["usuario"] = $this->loginModel->buscarUsuario($email , $password);
-        echo $this->render->render("view/pruebaView.php", $data);
+        $this->loginModel->buscarUsuario($email , $password);
+
     }
 }
