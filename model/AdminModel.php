6d38d6237bb5752bca5549f5fcb9b0e7c85232bc
AibarHernanEzequiel <aibarhernanezequiel@gmail.com>
2021-06-27 07:45:58 -0300

    Se añade un metodo existeMasDeUnaFila() el cual valida si la consulta a la bd devuelve mas de un 1 registro Se añade un metodo existeMasDeUnaFila() el cual valida si la consulta a la bd devuelve mas de un 1 registro

--- model/AdminModel.php
+++ model/AdminModel.php
@@ -13,21 +13,30 @@ class AdminModel{
         return $this->convertirArrayAsociativo($resultado);
     }
 
-    public function obtenerUsuarioPorId($id){
-        $sql = "SELECT * FROM usuario WHERE id = " . $id;
-        $resultado = $this->ejecutarConsulta($sql);
-        return $this->convertirArrayAsociativo($resultado);
-    }
-
     private function ejecutarConsulta($sql){
         return $this->database->execute($sql);
     }
 
     private function convertirArrayAsociativo($resultado_consulta){
-        $data = array();
-        while ($rows = $resultado_consulta->fetch_assoc()) {
-            $data[] = $rows;
+        if($this->existeMasDeUnaFila($resultado_consulta)){
+            $data = array();
+            while ($rows = $resultado_consulta->fetch_assoc()) {
+                $data[] = $rows;
+            }
+            return $data;
         }
-        return $data;
+    }
+
+    private function existeMasDeUnaFila($resultado_consulta){
+        if ($resultado_consulta->num_rows > 0) {
+            return true;
+        }
+            return false;
+    }
+
+    public function obtenerUsuarioPorId($id){
+        $sql = "SELECT * FROM usuario WHERE id = " . $id;
+        $resultado = $this->ejecutarConsulta($sql);
+        return $this->convertirArrayAsociativo($resultado);
     }
 }
