027edb1fb5c181ebbf5ca54c7477c62d14d9c12b
yedai13 <facundo.marin2012@gmail.com>
2021-06-27 06:11:13 -0300

    carga de camiones y arrastradores en bd

--- G7.sql
+++ G7.sql
@@ -191,17 +191,45 @@ CREATE TABLE camiones
 id tinyint primary key auto_increment,
 patente varchar(6) not null unique,
 marca varchar(15),
-modelo int(15),
+modelo varchar(15),
+nro_motor int(15) unique,
+nro_chasis varchar(15) unique,
 kilometraje int(15),
 ultimo_service datetime,
-nro_chasis int(15) unique,
-nro_motor int(15) unique,
 id_viaje tinyint,
 foreign key(id_viaje) references viaje(id_viaje)
 );
 
-INSERT into camiones(patente, marca, modelo, kilometraje, ultimo_service, nro_chasis, nro_motor, id_viaje)
-values('amh628', 'Scania', 2015, 10000, '20170920', 235, 14, 1);  
+INSERT into camiones(marca, modelo, patente, nro_motor, nro_chasis, kilometraje, ultimo_service, id_viaje)
+values('IVECO', 'Cursor', 'AA123CD', 53879558, 'L53879558', 10000),
+      ('IVECO','Cursor','AA124DC', 69904367,'R69904367', 10000),
+      ('IVECO','Cursor','AD200XS',	57193968,'R57193968', 10000),
+      ('IVECO','Cursor','AA211ZX',	82836641,'N82836641', 10000),
+      ('IVECO','Cursor','AC452WE',	28204636,'R28204636', 10000),
+      ('IVECO','Cursor','AA233SS',	26139668,'K26139668', 10000),
+      ('IVECO','Cursor','AB900QW',	44301415,'F44301415', 10000),
+      ('IVECO','Cursor','AC342WW',	44260023,'D44260023', 10000),
+
+      ('SCANIA','G310','AA150QW',	82039512,'I82039512', 10000),
+      ('SCANIA','G410','AB198QZ',	18389741,'V18389741', 10000),
+      ('SCANIA','G460','AC246QD',	62500687,'O62500687', 10000),
+      ('SCANIA','G310','AD294QW',	27510702,'T27510702', 10000),
+      ('SCANIA','G410','AA342QZ',	72582865,'C72582865', 10000),
+      ('SCANIA','G460','AB390QD',	32041290,'Z32041290', 10000),
+      ('SCANIA','G310','AC438QW',	54712451,'W54712451', 10000),
+      ('SCANIA','G410','AD486QZ',   56284263,'L56284263', 10000),
+      ('SCANIA','G460','AA534QD',	21357689,'A21357689', 10000),
+
+      ('M.BENZ','Actros 1846','AB582QW',17800122,'V17800122', 10000),
+      ('M.BENZ','Actros 1846','AC630QZ',88648319,'G88648319', 10000),
+      ('M.BENZ','Actros 1846','AD678QD',23849041,'C23849041', 10000),
+      ('M.BENZ','Actros 1846','AA726QW',54650513,'C54650513', 10000),
+      ('M.BENZ','Actros 1846','AB774QZ',46753468,'J46753468', 10000),
+      ('M.BENZ','Actros 1846','AC822QD',60916748,'J60916748', 10000),
+      ('M.BENZ','Actros 1846','AD870QW',30207594,'M30207594', 10000),
+      ('M.BENZ','Actros 1846','AA918QZ',31256965,'C31256965', 10000),
+      ('M.BENZ','Actros 1846','AB966QD',32632699,'B32632699', 10000),
+      ('M.BENZ','Actros 1846','AC989QW',64092078,'F64092078', 10000);
 
 CREATE TABLE arrastrador
 (id tinyint primary key auto_increment,
@@ -213,8 +241,52 @@ foreign key(patente) references camiones(patente),
 FOREIGN KEY (id_viaje) REFERENCES viaje(id_viaje)
 );
 
-INSERT into arrastrador(nro_chasis, tipo, patente)
-values(1123165616, 'Jaula', 'amh628');  
+INSERT into arrastrador( tipo, patente, nro_chasis)
+values('Araña','AA100AS',585822),
+      ('Araña','AC125AD',605737),
+      ('Araña','AB135AG',705687),
+      ('Araña','AD166AS',815082),
+      ('Araña','AA189AD',775167),
+      ('Araña','AC208AG',642287),
+      ('Araña','AB230AS',678666),
+      ('Araña','AD252AD',758967),
+      ('Araña','AA274AG',498515),
+
+      ('Jaula','AC296AS',882174),
+      ('Jaula','AB318AD',595287),
+      ('Jaula','AD340AG',549916),
+      ('Jaula','AA362AS',831768),
+      ('Jaula','AC383AD',535330),
+
+      ('Tanque','AB405AG',583419),
+      ('Tanque','AD427AS',703673),
+      ('Tanque','AA449AD',884654),
+      ('Tanque','AC471AG',510019),
+      ('Tanque','AB493AS',595948),
+      ('Tanque','AD515AD',704640),
+      ('Tanque','AA537AG',752105),
+
+      ('Granel','AB581AD',761560),
+      ('Granel','AD602AG',555608),
+      ('Granel','AA624AS',852157),
+      ('Granel','AC646AD',710797),
+      ('Granel','AB668AG',815072),
+      ('Granel','AD690AS',495851),
+      ('Granel','AA712AD',468708),
+      ('Granel','AC734AG',661897),
+      ('Granel','AB756AS',616372),
+      ('Granel','AD778AD',873758),
+      ('Granel','AA800AG',820810),
+
+      ('CarCarrier','AD100AZ',730027),
+      ('CarCarrier','AD100AQ',730502),
+      ('CarCarrier','AD100ER',730978),
+      ('CarCarrier','AD101EF',731453),
+      ('CarCarrier','AD102HG',731929),
+      ('CarCarrier','AD103LO',732404),
+      ('CarCarrier','AD104WE',732880),
+      ('CarCarrier','AD105ZP',733355);
+
 
 CREATE TABLE mantenimiento
 (id tinyint primary key auto_increment,
