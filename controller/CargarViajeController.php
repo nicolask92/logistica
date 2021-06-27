816af4eee7f3b3985803467fc1fefacaae3c4d22
yedai13 <facundo.marin2012@gmail.com>
2021-06-27 13:54:02 -0300

    update controller viaje

--- controller/CargarViajeController.php
+++ controller/CargarViajeController.php
@@ -26,14 +26,47 @@ class CargarViajeController
         $origen = $_POST["origenViaje"];
         $destino= $_POST["destinoViaje"];
         $fecha_carga= $_POST["fechaCarga"];
-        var_dump($fecha_carga);
         $id_supervisor= $_POST["supervisorViaje"];
-        $id_chofer= $_POST["choferViaje"];
+        $legajo_chofer= $_POST["choferViaje"];
         $id_camion= $_POST["camionViaje"];
         $id_arrastrador = $_POST["arrastradorViaje"];
 
         $this->cargarViajeModel->insertViaje($origen, $destino, $fecha_carga, $id_supervisor,
-                                                        $id_chofer,$id_camion,$id_arrastrador);
+                                                        $legajo_chofer,$id_camion,$id_arrastrador);
+
+        $id_viaje = $this->cargarViajeModel->ultimoId();
+        $nombre_cliente = $_POST["nombreCliente"];
+        $apellido_cliente = $_POST["apellidoCliente"];
+        $cuit_cliente = $_POST["cuitCliente"];
+        $domicilio_cliente =$_POST["domicilioCliente"];
+        $tel_cliente = $_POST["telefonoCliente"];
+        $email_cliente = $_POST["emailCliente"];
+
+        $this->cargarViajeModel->insertCliente($nombre_cliente, $apellido_cliente, $cuit_cliente, $domicilio_cliente,
+                                                $tel_cliente, $email_cliente, $id_viaje);
+
+        $eta=$_POST["eta"];
+        $etd=$_POST["etd"];
+        $km_p=$_POST["kmPrevisto"];
+        $combustible_p =$_POST["combustiblePrevisto"];
+        $viaticos_p=$_POST["viaticosPrevisto"];
+        $peajes_p=$_POST["peajesPrevisto"];
+        $pesajes_p=$_POST["pesajesPrevisto"];
+        $extras_p=$_POST["extrasPrevisto"];
+        $fee_p=$_POST["feePrevisto"];
+
+        $this->cargarViajeModel->insertCosteoPrevisto($id_viaje,$eta, $etd, $combustible_p, $km_p,$viaticos_p,$peajes_p,
+                                                        $pesajes_p, $extras_p, $fee_p);
+
+        $tipo_carga = $this->cargarViajeModel->getTipoCarga($id_arrastrador);
+
+        $hazard = $_POST["hazardCarga"];
+        $imo = $_POST["imoCarga"];
+        $reefer = $_POST["reeferCarga"];
+        $temperatura = $_POST["temperaturaCarga"];
+        $peso_neto = $_POST["pesoCarga"];
+
+        $this->cargarViajeModel->insertCarga($tipo_carga, $hazard, $imo, $reefer, $temperatura, $peso_neto, $id_viaje);
 
 
     }
