f747632bca920e8d454c80ab6f9f4f343429febe
yedai13 <facundo.marin2012@gmail.com>
2021-07-11 16:09:11 -0300

    add

--- view/detalleView.php
+++ view/detalleView.php
@@ -13,11 +13,88 @@
         <!-- Begin Page Content -->
         <div class="container-fluid">
 
-            <div class="alert alert-danger" role="alert">
+            <div class="row">
+                <div class="col-sm-6 mb-3 mb-sm-0">
 
-                ACA VA LA TABLA
+                    <table class="table table-hover">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Viaje</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">ID</th>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Origen</th>
+                            <td>Jacob</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Destino</th>
+                            <td>Larry</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Fecha carga</th>
+                            <td>Larry</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Chofer</th>
+                            <td>Larry</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Supervisor</th>
+                            <td>Larry</td>
+                        </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+
+                <div class="col-sm-6">
+
+                    <table class="table">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Carga</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">Tipo</th>
+                            <td>Mark</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Peso Neto</th>
+                            <td>Jacob</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Hazard</th>
+                            <td>Larry</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Imo Class</th>
+                            <td>Larry</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Reefer</th>
+                            <td>Larry</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Temperatura</th>
+                            <td>Larry</td>
+                        </tr>
+                        </tbody>
+                    </table>
+
+                </div>
 
             </div>
+
+
         </div>
     </div>
 
