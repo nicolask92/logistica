3cd483ad282234571d9fb3abca3d0f9e0472840c
yedai13 <facundo.marin2012@gmail.com>
2021-07-18 23:36:39 -0300

    update Mecanico

--- view/mecanicoView.php
+++ view/mecanicoView.php
@@ -1,82 +1,90 @@
 {{> header }}
 {{> barraLateral }}
-    
-            <!-- Content Wrapper -->
-            <div id="content-wrapper" class="d-flex flex-column">
 
-                <!-- Main Content -->
-                <div id="content">
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
 
-                    <!-- Topbar -->
-                    {{> barraTop }}
-					<!-- End of Topbar -->
+    <!-- Main Content -->
+    <div id="content">
 
-                    <!-- Begin Page Content -->
-                    <div class="container-fluid">
+        <!-- Topbar -->
+        {{> barraTop }}
+        <!-- End of Topbar -->
 
-<div class="{{alerta}}" role="alert">
-{{mensaje}}
-</div>
-                        
-                        <!-- Page Heading -->
-                        <h1 class="h3 mb-4 text-gray-800 text-center">Datos del mantenimiento</h1>
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
 
-                        <div class="achicar">
-                            <H3>Mecánico</H3>
+            <div class="{{alerta}}" role="alert">
+                {{mensaje}}
+            </div>
 
-                            <form method="POST" enctype="multipart/form-data" action="mecanico/procesarMantenimiento">
-                                <div class="form-group row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input type="text" class="form-control" name="idMecanico"
-                                            id="idMecanico" placeholder="Legajo">
-                                    </div>
-                                </div>    
+            <!-- Page Heading -->
+            <h1 class="h3 mb-4 text-gray-800 text-center">Datos del mantenimiento</h1>
 
-                                <div>
-                                    <hr class="sidebar-divider mt-4">
-                                    <h3>Vehículo</h3>
-                                </div>
+            <div class="achicar">
+                <H3>Mecánico</H3>
 
-                                <div class="form-group">
-                                    <input type="text" class="form-control" id="idVehiculo"
-                                        name="idVehiculo"    placeholder="ID">
-                                </div>
-                                
-                                <div class="form-group">
-                                <label for="select">Service</label>
-                                <select id="serviceInternoExterno" name="serviceInternoExterno" class="form-control">
-                                    <option value="interno">Interno</option>
-                                    <option value="externo" selected>Externo</option>
-                                </select>    
-                                </div>
-
-                                <div class="form-group">
-                                    <input type="date" class="form-control" id="fechaService"
-                                        name="fechaService" aria-describedby="fecha">
-                                    <small id="fechaService" class="text-muted">
-                                        Fecha de service
-                                    </small>
-                                </div>
-
-                                <div class="form-group">
-                                    <input type="text" class="form-control" id="repuestoCambiado"
-                                        name="repuestoCambiado"    placeholder="Repuesto cambiado">
-                                </div>
-
-                                <div class="form-group">
-                                    <input type="text" class="form-control" id="costoVehiculo"
-                                        name="costoVehiculo"    placeholder="Costo">
-                                </div>
-
-
-                                <div class="form-group text-center">
-                                    <button type="submit" class="btn btn-dark">Enviar</button>
-                                </div>
-                            </form>
-                        </div>
+                <form method="POST" enctype="multipart/form-data" action="mecanico/procesarMantenimiento">
+                    <div class="form-group">
+                        <select class="form-control" id="datosMecanico" name="datosMecanico">
+                            <option selected value="0" >Seleccionar Mecánico</option>
+                            {{#datosMecanico}}
+                            <option value="{{id}}">{{nombre}} {{apellido}} </option>
+                            {{/datosMecanico}}
+                        </select>
                     </div>
-                    <!-- /.container-fluid -->
-                </div>
-                <!-- End of Main Content -->
+                    {{#errorDatosMecanico}}<p class="text-danger">Debe Seleccionar un mecanico</p>{{/errorDatosMecanico}}
 
-                {{> footer }}
+                    <div>
+                        <hr class="sidebar-divider mt-4">
+                        <h3>Vehículo</h3>
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control" id="seleccionarVehiculo" name="seleccionarVehiculo">
+                            <option selected value="0" >Seleccionar Vehículo</option>
+                            {{#seleccionarVehiculo}}
+                            <option value="{{id}}">{{marca}}-{{modelo}} </option>
+                            {{/seleccionarVehiculo}}
+                        </select>
+                        {{#errorSeleccionarVehiculo}}<p class="text-danger">Debe Seleccionar un Vehículo</p>{{/errorSeleccionarVehiculo}}
+                    </div>
+
+                    <div class="form-group">
+                        <label for="select">Service</label>
+                        <select id="tipoService" name="tipoService" class="form-control" value="{{tipoService}}">
+                            <option selected value="0" >Seleccionar service</option>
+                            <option value="interno">Interno</option>
+                            <option value="externo" selected>Externo</option>
+                        </select>
+                        {{#errorTipoService}}<p class="text-danger">Debe Seleccionar un tipo de service</p>{{/errorTipoService}}
+                    </div>
+
+                    <div class="form-group">
+                        <input type="date" class="form-control" id="fechaService" name="fechaService" value="{{fechaService}}" aria-describedby="fecha">
+                        <small id="fechaService" class="text-muted">
+                            Fecha de service
+                        </small>
+                        {{#errorFechaService}}<p class="text-danger">Debe Seleccionar una fecha</p>{{/errorFechaService}}
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="repuestoCambiado" name="repuestoCambiado" value="{{repuestoCambiado}}"   placeholder="Repuesto cambiado">
+                        {{#errorRepuestoCambiado}}<p class="text-danger">Debe rellenar este campo</p>{{/errorRepuestoCambiado}}
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="costoVehiculo"  name="costoVehiculo" value="{{costoVehiculo}}"   placeholder="Costo">
+                        {{#errorCostoVehiculo}}<p class="text-danger">Debe rellenar este campo</p>{{/errorCostoVehiculo}}
+                    </div>
+
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-dark">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- End of Main Content -->
+
+    {{> footer }}
